e928e39062d565b2737106a97e6bbdf3032de1be
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-06-15 19:51:34 -0500

    Add Product class

---
+++ src/main/java/com/pluralsight/northwind_api/model/Product.java
@@ -0,0 +1,121 @@
+package com.pluralsight.northwind_api.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "Products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ProductID")
+    private Long productId;
+
+    @Column(name = "ProductName")
+    private String productName;
+
+    @Column(name = "SupplierID")
+    private int supplierId;
+
+    @Column(name = "CategoryID")
+    private int categoryId;
+
+    @Column(name = "QuantityPerUnit")
+    private String quantityPerUnit;
+
+    @Column(name = "UnitPrice")
+    private BigDecimal unitPrice;
+
+    @Column(name = "UnitsInStock")
+    private int unitsInStock;
+
+    @Column(name = "UnitsOnOrder")
+    private int unitsOnOrder;
+
+    @Column(name = "ReorderLevel")
+    private int reorderLevel;
+
+    @Column(name = "Discontinued")
+    private boolean discontinued;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(int supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getQuantityPerUnit() {
+        return quantityPerUnit;
+    }
+
+    public void setQuantityPerUnit(String quantityPerUnit) {
+        this.quantityPerUnit = quantityPerUnit;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(int unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
+
+    public int getUnitsOnOrder() {
+        return unitsOnOrder;
+    }
+
+    public void setUnitsOnOrder(int unitsOnOrder) {
+        this.unitsOnOrder = unitsOnOrder;
+    }
+
+    public int getReorderLevel() {
+        return reorderLevel;
+    }
+
+    public void setReorderLevel(int reorderLevel) {
+        this.reorderLevel = reorderLevel;
+    }
+
+    public boolean isDiscontinued() {
+        return discontinued;
+    }
+
+    public void setDiscontinued(boolean discontinued) {
+        this.discontinued = discontinued;
+    }
+}
